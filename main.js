8768f69b9c59f8a5fbfec976d909024671ba651f
srujan-j24 <srujanj2021@gmail.com>
2023-10-15 11:20:03 +0530

    fixed the regex to add only valid name-value pair

--- main.js
+++ main.js
@@ -1,49 +1,65 @@
-let list = document.getElementById("items");
+let dom_list = document.getElementById("items");
 let list_inpt = document.getElementById("inpt");
-let pattern =  /([^\s]*)\s(\d+(\.\d+)?)$/;
-let items_list = {}
+let pattern = /([^\s]*)\s+(\d+(\.\d+)?)$/;
+let items_list = {};
+let inpt_label = document.querySelector("label");
+let error_value = false;
+const label_text = "Enter name & value seperated by space :";
 
-
-list_inpt.addEventListener("keyup", (e)=>{
-  if(e.key == "Enter"){
-    let inpt_val = list_inpt.value.trim();
-    let m_ary= inpt_val.match(pattern);
-    if(m_ary){
-      let name = m_ary[1];
-      let val = m_ary[2];
-      if(name in items_list){
-        
-      }else{
-
-      }
-    }else{
-      list_inpt.classList.add("error")
-      setTimeout(()=>{list_inpt.classList.remove("error")}, 500);
-    }
-  }
-  
-
-})
-
-function addNewItem(name, value){
-  let new_item = document.createElement("div");
-  new_item.classList.add("item");
-  new_item.id = name;
-  list.appendChild(new_item);
+function createItem(name, value) {
+	let newDiv = document.createElement("div");
+	let text = document.createElement("h4");
+	text.innerText = `${name} : ${value}`;
+	newDiv.appendChild(text);
+	newDiv.classList.add("item");
+	return newDiv;
 }
 
+function valuateInput(value) {
+	value = value.trim();
+	let matchObject = value.match(pattern);
+	return matchObject;
+}
 
+function showError(message) {
+	if(!error_value){
+		inpt_label.innerText = message;
+		list_inpt.classList.add("error");
+	}
+	list_inpt.classList.add("error-animation");
+	list_inpt.addEventListener("animationend", ()=>{
+		list_inpt.classList.remove("error-animation");
+	});
+	error_value = true;
+}
 
+list_inpt.addEventListener("keyup", (e) => {
+	if(error_value){
+		console.log("hi");
+		inpt_label.innerText = label_text;
+		list_inpt.classList.remove("error");
+		error_value = false;
+	}
 
-// const input = "word123,42";
-// const pattern = /(\w+),(\d+)/;
-// const match = input.match(pattern);
+	if(!(e.key == "Enter")){
+		return;
+	}
 
-// if (match) {
-//   const word = match[1];
-//   const number = match[2];
-//   console.log("Word:", word);
-//   console.log("Number:", number);
-// } else {
-//   console.log("No match found.");
-// }
+	let match_obj = valuateInput(list_inpt.value);
+	if (!match_obj) {
+		console.log("bi");
+		showError("Invalid input  !!!");
+		return;
+	}
+	let new_name = match_obj[1];
+	let new_val = Number(match_obj[2]);
+	if(new_name in items_list){
+		showError("This name already exists  !!!");
+		return;
+	}
+	let newItem = createItem(new_name, new_val);
+	dom_list.prepend(newItem);
+	list_inpt.value = ""
+
+	
+});
