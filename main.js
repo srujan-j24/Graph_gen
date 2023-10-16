fb84e314ce6cca82655f527f517d9f8078a7b2c4
srujan-j24 <srujanj2021@gmail.com>
2023-10-16 23:30:06 +0530

    side-in + fade-in for items added

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 let dom_list = document.getElementById("items");
 let list_inpt = document.getElementById("inpt");
+let no_items_div = document.getElementById("noitems");
 let pattern = /([^\s]*)\s+(\d+(\.\d+)?)$/;
 let items_list = {};
 let inpt_label = document.querySelector("label");
 let error_value = false;
+let current_item;
 const label_text = "Enter name & value seperated by space :";
 
 function createItem(name, value) {
-	let newDiv = document.createElement("div");
+	current_item = document.createElement("div");
 	let text = document.createElement("h4");
 	text.innerText = `${name} : ${value}`;
-	newDiv.appendChild(text);
-	newDiv.classList.add("item");
-	return newDiv;
+	current_item.appendChild(text);
+	current_item.classList.add("item");
 }
 
 function valuateInput(value) {
@@ -31,6 +32,36 @@ function showError(message) {
 		list_inpt.classList.remove("error-animation");
 	});
 	error_value = true;
+}
+function assistTransition(){
+	dom_list.prepend(current_item);
+	dom_list.classList.remove("make-space");
+	current_item.classList.add("fade-in-animation");
+	current_item.addEventListener("animationend", ()=>{
+		current_item.style.opacity = 1;
+	});
+	dom_list.removeEventListener("animationend", assistTransition);
+}
+function addItem(new_name,new_val){
+	if(new_name in items_list){
+		showError("This name already exists  !!!");
+		return;
+	}
+	if(Object.keys(items_list).length === 0){
+		no_items_div.style.display = "none";
+	}
+	createItem(new_name, new_val);
+	dom_list.classList.add("make-space");
+	list_inpt.value = "";
+	dom_list.addEventListener("animationend", assistTransition);
+	
+
+	items_list[new_name] = {
+		name: new_name,
+		value: new_val,
+		dom_obj: current_item
+	}
+	console.log(items_list);
 }
 
 list_inpt.addEventListener("keyup", (e) => {
@@ -51,15 +82,6 @@ list_inpt.addEventListener("keyup", (e) => {
 		showError("Invalid input  !!!");
 		return;
 	}
-	let new_name = match_obj[1];
-	let new_val = Number(match_obj[2]);
-	if(new_name in items_list){
-		showError("This name already exists  !!!");
-		return;
-	}
-	let newItem = createItem(new_name, new_val);
-	dom_list.prepend(newItem);
-	list_inpt.value = ""
 
-	
+	addItem(match_obj[1], Number(match_obj[2]));
 });
